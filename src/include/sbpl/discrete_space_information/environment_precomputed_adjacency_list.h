21d074731cddd502e59008c5aa3ae33ac4255913
Pyo <pyo@robotis.com>
2018-08-07 07:42:14 +0900

    Rebase from 'debian/melodic/sbpl'

--- src/include/sbpl/discrete_space_information/environment_precomputed_adjacency_list.h
+++ src/include/sbpl/discrete_space_information/environment_precomputed_adjacency_list.h
@@ -1,3 +1,32 @@
+/*
+ * Copyright (c) 2008, Maxim Likhachev
+ * All rights reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright
+ *       notice, this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright
+ *       notice, this list of conditions and the following disclaimer in the
+ *       documentation and/or other materials provided with the distribution.
+ *     * Neither the name of the Carnegie Mellon University nor the names of its
+ *       contributors may be used to endorse or promote products derived from
+ *       this software without specific prior written permission.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE
+ * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+ * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+ * SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+ * INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+ * CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+ * ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+ * POSSIBILITY OF SUCH DAMAGE.
+ */
+
 #ifndef __PRECOMPUTED_ADJACENCY_LIST_H_
 #define __PRECOMPUTED_ADJACENCY_LIST_H_
 
@@ -310,9 +339,7 @@ void AdjacencyListSBPLEnv<Coords>::SetAllActionsandAllOutcomes(CMDPSTATE* state)
 template<class Coords>
 void AdjacencyListSBPLEnv<Coords>::SetAllPreds(CMDPSTATE* state)
 {
-    // Apparently this is not always necessary
-    std::cout << "Error: SetAllPreds not implemented for adjacency list";
-    throw new SBPL_Exception();
+    throw SBPL_Exception("Error: SetAllPreds not implemented for adjacency list");
 }
 
 template<class Coords>
@@ -335,8 +362,7 @@ void AdjacencyListSBPLEnv<Coords>::GetSuccs(int SourceStateID, std::vector<int>*
 template<class Coords>
 void AdjacencyListSBPLEnv<Coords>::GetPreds(int TargetStateID, std::vector<int>* PredIDV, std::vector<int>* CostV)
 {
-    std::cout << "Error: GetPreds not currently implemented for adjacency list";
-    throw new SBPL_Exception();
+    throw SBPL_Exception("Error: GetPreds not currently implemented for adjacency list");
 }
 
 template<class Coords>
